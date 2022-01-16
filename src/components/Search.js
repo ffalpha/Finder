3c24c3922983cdbbb49e4f1aff1956c5a4db2103
ffalpha <kalanam217@gmail.com>
2022-01-16 13:39:42 +0530

    Add new pakacage

---
+++ src/components/Search.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { useDebounce } from 'use-debounce';
+function Search() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Search
